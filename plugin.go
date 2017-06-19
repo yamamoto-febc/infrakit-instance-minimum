2e035ef8b1e0b99293cd5d76f6265b9a41818366
febc.yamamoto <yamamoto.febc@gmail.com>
2017-06-19 21:19:05 +0900

    Implements Provision

--- plugin.go
+++ plugin.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/docker/infrakit/pkg/spi/instance"
 	"github.com/docker/infrakit/pkg/types"
+	"math/rand"
+	"os"
+	"path/filepath"
+)
+
+var (
+	instanceDir = "/tmp/infrakit-dummy-instances"
 )
 
 type plugin struct{}
@@ -16,7 +24,28 @@ func (p *plugin) Validate(req *types.Any) error {
 }
 
 func (p *plugin) Provision(spec instance.Spec) (*instance.ID, error) {
-	return nil, nil
+	// ランダムなIDを生成
+	id := instance.ID(fmt.Sprintf("instance-%d", rand.Int63()))
+	path := filepath.Join(instanceDir, string(id))
+
+	// ディレクトリ(/tmp/infrakit-dummy-instances)がなければ作成
+	_, err := os.Stat(instanceDir)
+	if err != nil {
+		err := os.MkdirAll(instanceDir, os.FileMode(0777))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// ファイル作成
+	if _, err := os.Stat(path); err != nil {
+		f, err := os.Create(path)
+		if err != nil {
+			return nil, err
+		}
+		defer f.Close()
+	}
+	return &id, nil
 }
 
 func (p *plugin) Label(instance instance.ID, labels map[string]string) error {
